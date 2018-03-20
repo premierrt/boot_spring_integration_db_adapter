34355f5c31f30b548bd60aa0f0aa578a870827b7
dom_linux <dom_linux@ddddd.xy>
2018-03-20 11:54:29 +0100

    init xsd

---
+++ src/main/java/rt/eureka/SpringBootDbAdapterApplication.java
@@ -0,0 +1,12 @@
+package rt.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDbAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDbAdapterApplication.class, args);
+	}
+}
