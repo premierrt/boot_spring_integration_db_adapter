932403e47ecd4046ce9def0a456e963d1f8373be
dom_linux <dom_linux@ddddd.xy>
2018-03-20 22:24:58 +0100

    restcontroler

--- src/main/java/rt/eureka/demo/DemoAddKomponetRestController.java
+++ src/main/java/rt/eureka/demo/DemoAddKomponetRestController.java
@@ -1,5 +1,25 @@
 package rt.eureka.demo;
 
-public class DemoAddKomponetRestController {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/komponent")
+public class DemoAddKomponetRestController {
+	
+	private static Log log = LogFactory.getLog(DemoKomponentyService.class);
+	
+	@Autowired
+	private DemoKomponentyService demoKomponentyService;
+
+	@RequestMapping(method=RequestMethod.POST)
+	public void addKomponent(String name){
+		log.info("jedzie addKomponent" );
+		demoKomponentyService.saveKomponent(name);
+		
+	}
 }
